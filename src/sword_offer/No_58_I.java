54c4308b168844524f31720844679701a8117fe6
weib-ls <vb_ahh@foxmail.com>
2021-12-14 19:19:39 +0800

    剑指 Offer  58 - I. 翻转单词顺序

---
+++ src/sword_offer/No_58_I.java
@@ -0,0 +1,37 @@
+package sword_offer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author weib
+ * @date 2021-12-14 19:18
+ * 58 - I. 翻转单词顺序
+ * https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof/
+ */
+public class No_58_I {
+    public String reverseWords(String s) {
+        if (s.equals("")) {
+            return "";
+        }
+        String[] result = s.split(" ");
+        List<String> rList = new ArrayList<>();
+        for (int i = result.length - 1; i >=0; i--) {
+            String s1 = result[i];
+            if("".equals(s1) || " ".equals(s1) || s1 == null){
+                continue;
+            }
+            rList.add(" ");
+            rList.add(s1);
+        }
+        if (rList.size() > 0 ) {
+            rList.remove(0);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s1 : rList) {
+            sb.append(s1);
+        }
+        return sb.toString();
+    }
+}
