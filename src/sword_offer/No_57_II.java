51e5b41d22b4abf5b4ed9850ae3c8c813513de81
weib-ls <vb_ahh@foxmail.com>
2021-12-28 22:27:36 +0800

    剑指 Offer 57 - II. 和为s的连续正数序列

---
+++ src/sword_offer/No_57_II.java
@@ -0,0 +1,36 @@
+package sword_offer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author weib
+ * @date 2021-12-28 22:25
+ * 57 - II. 和为s的连续正数序列
+ * https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/
+ * 直接暴力吧
+ */
+public class No_57_II {
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> vec = new ArrayList<int[]>();
+        int sum = 0, limit = (target - 1) / 2; // (target - 1) / 2 等效于 target / 2 下取整
+        for (int x = 1; x <= limit; ++x) {
+            long delta = 1 - 4 * (x - (long) x * x - 2 * target);
+            if (delta < 0) {
+                continue;
+            }
+            int delta_sqrt = (int) Math.sqrt(delta + 0.5);
+            if ((long) delta_sqrt * delta_sqrt == delta && (delta_sqrt - 1) % 2 == 0) {
+                int y = (-1 + delta_sqrt) / 2; // 另一个解(-1-delta_sqrt)/2必然小于0，不用考虑
+                if (x < y) {
+                    int[] res = new int[y - x + 1];
+                    for (int i = x; i <= y; ++i) {
+                        res[i - x] = i;
+                    }
+                    vec.add(res);
+                }
+            }
+        }
+        return vec.toArray(new int[vec.size()][]);
+    }
+}
