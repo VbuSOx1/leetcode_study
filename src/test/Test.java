c66ee2f255a8001b4de65f0747a91de290e477b5
weib-ls <vb_ahh@foxmail.com>
2022-03-18 10:01:05 +0800

    热题 100 33. 搜索旋转排序数组

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author weib
  * @date 2021-12-15 10:36
@@ -7,8 +11,15 @@ package test;
 public class Test {
     @org.junit.Test
     public void test(){
-        String s = "123456789";
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        List<String> input = new ArrayList<>();
+        while (scanner.hasNext()) {
+            input.add(scanner.next());
+        }
+        for (int i = 0; i < t; i++) {
+            
+        }
 
     }
 }
