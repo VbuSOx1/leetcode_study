fa9d49618f09664a94fe256353b7aff27a720b61
weib-ls <vb_ahh@foxmail.com>
2022-03-22 14:29:14 +0800

    热题 100 55. 跳跃游戏

---
+++ src/hot_100/No_55_jump_game.java
@@ -0,0 +1,38 @@
+package hot_100;
+
+/**
+ * @author weib
+ * @date 2022-03-22 14:21
+ * 55. 跳跃游戏
+ * https://leetcode-cn.com/problems/jump-game/
+ * 从后往前找0
+ */
+public class No_55_jump_game {
+    public static boolean canJump(int[] nums) {
+        // 找 0
+        if (nums.length == 1) {
+            return true;
+        }
+
+        for (int i = nums.length-1; i >=0 ; i--){
+            if (nums[i] != 0) {
+                continue;
+            }
+            int j = 0;
+            for (j = i-1; j >= 0; j--) {
+                if (nums[j] > i - j || (nums[j] == i-j && i == nums.length-1)){
+                    i = j;
+                    break;
+                }
+            }
+            if (j == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canJump(new int[]{1,0,1,0}));
+    }
+}
